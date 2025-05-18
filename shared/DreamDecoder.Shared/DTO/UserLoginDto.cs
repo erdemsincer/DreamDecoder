00b79fede595a05ca5a34e881d785a8d8cd99711
Erdem <erdemsincer123@icloud.com>
2025-05-18 23:25:41 +0300

    shared

---
+++ shared/DreamDecoder.Shared/DTO/UserLoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DreamDecoder.Shared.DTO;
+
+public class UserLoginDto
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
---
+++ shared/DreamDecoder.Shared/DTO/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DreamDecoder.Shared.DTO;
+
+public class UserRegisterDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
